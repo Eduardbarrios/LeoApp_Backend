67064489e2e26bb2db1282cc9b913229e2786df5
eduard09 <eduardbarrios0903@gmail.com>
2023-10-20 10:30:06 -0500

    Comercio en linea app Backend api V01

--- src/Models/db.js
+++ src/Models/db.js
@@ -1,31 +1,3 @@
-/* const { Client } = require("pg");
-require("dotenv").config();
-
-// Create a new Client object from the pg module to manage database connections
-const client = new Client({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	ssl: {
-		rejectUnauthorized: false, // Configuración para permitir conexiones SSL no autorizadas en Azure
-	},
-}); 
-
-// Connect to the database server
-client
-	.connect()
-	.then(() => {
-		console.log("Conexión exitosa a la base de datos");
-		// Aquí puedes ejecutar tus consultas SQL o realizar operaciones en la base de datos
-	})
-	.catch((error) => {
-		console.error("Error al conectar a la base de datos", error);
-	});
-
-module.exports = client;*/
-
 const mysql = require("mysql2");
 require("dotenv").config();
 
